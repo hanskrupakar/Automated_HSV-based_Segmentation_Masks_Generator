51aa22637cf7c74ced452b7c531c19a011f0559b
hanskrupakar <hanskrupakar1994@gmail.com>
2018-03-04 15:36:13 -0500

    removed GUI elements

--- sample.py
+++
@@ -1,8 +0,0 @@
-import glob, os
-import shutil
-
-for folder in ['hans_0/*', 'hans_1/*', 'hans_2/*']:
-	
-	for i, f in enumerate(sorted(glob.glob(folder))):
-		if(i%5==0):
-			shutil.copyfile(f, folder.split('/')[0]+'-'+f.split('/')[-1])
